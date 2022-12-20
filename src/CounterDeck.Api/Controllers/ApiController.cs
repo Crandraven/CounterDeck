6031525bf084087e0b2ccd52d314def2adba7679
Crandraven <laioscrandraven@gmail.com>
2022-12-20 21:06:51 +0100

    FluentValidator package implementation

--- src/CounterDeck.Api/Controllers/ApiController.cs
+++ src/CounterDeck.Api/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 using CounterDeck.Api.Common.http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CounterDeck.Api.Controllers;
 
@@ -17,10 +18,23 @@ public class ApiController : ControllerBase
     }
     protected IActionResult Problem(List<Error> errors)
     {
-        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
-        var firstError = errors[0];
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
 
-        var statusCode = firstError.Type switch
+        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             ErrorType.Validation => StatusCodes.Status400BadRequest,
@@ -28,6 +42,17 @@ public class ApiController : ControllerBase
             _ => StatusCodes.Status500InternalServerError
         };
 
-        return Problem(statusCode: statusCode, title: firstError.Description);
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionnary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionnary.AddModelError(error.Code, error.Description);
+        }
+        return ValidationProblem(modelStateDictionnary);
     }
 }
--- src/CounterDeck.Application/DependencyInjection.cs
+++ src/CounterDeck.Application/DependencyInjection.cs
@@ -1,3 +1,9 @@
+using System.Reflection;
+using CounterDeck.Application.Authentication.Commands.Register;
+using CounterDeck.Application.Authentication.Common;
+using CounterDeck.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +14,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(typeof(DependencyInjection).Assembly);
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidateBehavior<,>));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
